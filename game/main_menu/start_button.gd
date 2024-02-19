5dd7842f349288f2ba928d49ba5ef4b0f6cfd026
ZevZX <zevsoniczs25@gmail.com>
2024-02-19 23:22:11 +0100

    snake beta

--- game/main_menu/start_button.gd
+++ game/main_menu/start_button.gd
@@ -12,4 +12,4 @@ func _process(delta):
 
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://game/tic_tac_toe/tic_tac_toe.tscn")
+	get_tree().change_scene_to_file("res://game/snake/snake.tscn")
